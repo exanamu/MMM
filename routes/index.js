7d68a2a3cfcc68746741480f26488a4d1746b9d9
nmynje <exanamu@gmail.com>
2017-08-22 09:55:38 +0900

    Room used

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express= require('express');
 var router= express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.get('/chat', function(req, res, next) {
   res.render('chat');
+});
+
+app.get('/:room', function(req,res){
+    console.log('room name is :'+req.params.room);
+    res.render('chat', {room:req.params.room});
 });
 
 module.exports= router;
